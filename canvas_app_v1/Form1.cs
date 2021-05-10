ea03e6a503573cb2e1df0eb1b3f3cc80307f4069
samuel miller <samuel.miller@student.centralia.edu>
2021-05-10 11:32:36 -0700

    Add Nuget packages and include required classes in form code

--- canvas_app_v1/Form1.cs
+++ canvas_app_v1/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace canvas_app_v1
 {
